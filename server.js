b5efa16fb7daa5d61c26ba85c10ee7bdd28fbec2
doctortreadway <163783706+doctortreadway@users.noreply.github.com>
2025-03-27 12:35:28 -0500

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const axios = require('axios');
 
-const app = express(); // ✅ THIS is what was missing
+const app = express(); // 
 
 app.use(cors());
 
